f62718fc90771f6d34ca7abf35fe8ea3cc3246c5
Charalambos Isaias <isaias@eAdvertise.eu>
2026-08-06 12:19:15 +0300

    Remove MyShopify purchase-code verification

--- myshopify/views/admin/verify.php
+++ myshopify/views/admin/verify.php
@@ -4,26 +4,19 @@
    <div class="content">
       <div class="row">
          <div class="col-md-6 col-md-offset-3">
-            <h4 class="tw-mt-0 tw-font-semibold tw-text-lg tw-text-neutral-700">
-               <?php echo _l('myshopify_verify') ?>
-            </h4>
-            <?php echo form_open($this->uri->uri_string()); ?>
             <div class="panel_s">
                <div class="panel-body">
-                 
-                  <?php echo render_input('settings[myshopify_purchase_code]', 'purchase_code', get_option('myshopify_purchase_code'), 'text',['required'=>'required']); ?>
-                  <label for="show_subscriptions_in_customers_area" class="control-label clearfix">
-        <i class="fa-regular fa-circle-question" data-toggle="tooltip" data-title="How to find your purchase code" data-original-title="" title=""></i> <a href="https://help.market.envato.com/hc/en-us/articles/202822600-Where-Is-My-Purchase-Code" target="_blank">Where Is My Purchase Code?</a></label>
-               </div>
-               <div class="panel-footer text-right">
-                  <button class="btn btn-primary"
-                     type="submit"><?php echo _l('Save'); ?></button>
+                  <h4 class="tw-mt-0 tw-font-semibold tw-text-lg tw-text-neutral-700">
+                     <?php echo _l('myshopify_verify'); ?>
+                  </h4>
+                  <p class="text-muted">External purchase-code verification is disabled for this fork.</p>
+                  <a href="<?php echo admin_url('myshopify/products'); ?>" class="btn btn-primary">
+                     <?php echo _l('continue'); ?>
+                  </a>
                </div>
             </div>
-            <?php echo form_close(); ?>
          </div>
       </div>
    </div>
 </div>
 <?php init_tail(); ?>
-</html>
