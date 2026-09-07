fbfba14d99f87317e4c1b44fd87d194353d95bc3
Charalambos Isaias <isaias@eAdvertise.eu>
2026-09-07 10:24:36 +0300

    Add Commission documentation as a sidebar menu item

---
+++ commission/migrations/108_version_108.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Version_108 extends App_module_migration
+{
+    public function up()
+    {
+        // Version 1.0.8 adds bundled documentation and does not change the
+        // database schema. This migration intentionally has no database work.
+    }
+}
